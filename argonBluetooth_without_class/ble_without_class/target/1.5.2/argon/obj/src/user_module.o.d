f0ac5b6cde528d81cb1a6edcb8d69445ecc65a64
just2212 <just22@live.dk>
2020-11-11 10:24:01 +0100

    deleted BLE as it won't work to out  purpose

--- argonBluetooth_without_class/ble_without_class/target/1.5.2/argon/obj/src/user_module.o.d
+++
@@ -1,8 +0,0 @@
-c:/Users/Keld/Documents/PlatformIO/Projects/argonBluetooth_without_class/ble_without_class/target/1.5.2/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-
-../../../dynalib/inc/dynalib.h:
